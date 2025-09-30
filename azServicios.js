a3c2144e750c91459dbbffbade0630e621746664
OscarAxel <84112338+OscarAxel@users.noreply.github.com>
2025-09-30 15:32:41 -0600

    “AZren09D123456_S_L1_luz08_upd”

--- azServicios.js
+++
@@ -1,8 +0,0 @@
-function mostrarOcultarImagen(imgId){
-    var imagen = document.getElementById(imgId);
-    if(imagen.style.display === "none"){
-        imagen.style.display = "block";
-    } else {
-        imagen.style.display = "none";
-    }
-}
--- js/azServicios.js
+++ js/azServicios.js
@@ -6,3 +6,22 @@ function mostrarOcultarImagen(imgId){
         imagen.style.display = "none";
     }
 }
+function renovarContrato(espacioId,vigencia){
+    let palabras= espacioId.split(' ');
+    let inmueble= palabras[0];
+    let espacio= palabras[1];
+    let espacioDesc= '';
+    if(espacio.startsWith('S')){ espacioDesc= 'salón'; }
+    if(espacio.startsWith('D')){ espacioDesc= 'departamento'; }
+    if(espacio.startsWith('L')){ espacioDesc= 'local'; }
+    if(espacio.startsWith('E')){ espacioDesc= 'estacionamiento'; }
+    let respuesta= confirm('¿Enviar solicitud de renovación de contrato del '+espacioDesc+' '+espacio.substring(1)+'?');
+    if(respuesta){
+        alert('Por favor ten lista la siguiente información: plazo deseado en meses, pueden ser 6 o 12.');
+        mail(espacioId,'Renovación de contrato '+espacioId,'Plazo: 12 meses.');
+    }else{ alert(vigencia); }
+}
+function mail(espacioId,subject,msg){
+    var myMailTo= 'mailto:systemklx@gmail.com?cc=lexossoxel@me.com&subject=Gif SM ONLINE: ';
+    window.location.href= myMailTo+subject+'&body='+msg+'%0A%0ASaludos!';
+}
